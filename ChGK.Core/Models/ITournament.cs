360c4fa07535b7f80f45a0c0666ec8866a61bf5e
foxanna <anna.domashych@gmail.com>
2014-07-28 13:57:29 +0300

    Display tournament date in one single line (fix)

--- ChGK.Core/Models/ITournament.cs
+++ ChGK.Core/Models/ITournament.cs
@@ -4,7 +4,7 @@ using System.Collections;
 
 namespace ChGK.Core.Models
 {
-	public interface ITournament : IEnumerable<ITour>
+	public interface ITournament
 	{
 		string FileName { get; }
 
@@ -28,16 +28,6 @@ namespace ChGK.Core.Models
 		public string PlayedAt { get; set; }
 
 		public List<ITour> Tours { get; set; }
-
-		public IEnumerator<ITour> GetEnumerator ()
-		{
-			return Tours.GetEnumerator ();
-		}
-
-		IEnumerator IEnumerable.GetEnumerator ()
-		{
-			return Tours.GetEnumerator ();
-		}
 	}
 }
 
--- ChGK.Core/ViewModels/TournamentViewModel.cs
+++ ChGK.Core/ViewModels/TournamentViewModel.cs
@@ -1,15 +1,20 @@
 ﻿using Cirrious.MvvmCross.ViewModels;
 using ChGK.Core.Models;
+using System.Collections.Generic;
+using System.Collections;
 
 namespace ChGK.Core.ViewModels
 {
-	public class TournamentViewModel: MvxViewModel
+	public class TournamentViewModel : MvxViewModel, IEnumerable<ITour>
 	{
+		List<ITour> Tours { get; set; }
+
 		public TournamentViewModel (ITournament tournament)
 		{
 			Name = tournament.Name;
 			FileName = tournament.FileName;
 			Dates = string.Format ("Отыгран {0}  Добавлен {1}", tournament.PlayedAt, tournament.AddedAt);
+			Tours = tournament.Tours;
 		}
 
 		public string Name { get; set; }
@@ -17,6 +22,16 @@ namespace ChGK.Core.ViewModels
 		public string FileName { get; set; }
 
 		public string Dates { get; set; }
+
+		public IEnumerator<ITour> GetEnumerator ()
+		{
+			return Tours.GetEnumerator ();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator ()
+		{
+			return Tours.GetEnumerator ();
+		}
 	}
 }
 
