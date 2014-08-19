42bc6099f82e17621952834236fc7fda3e9af955
foxanna <anna.domashych@gmail.com>
2014-08-19 17:25:18 +0300

    Email plugin

---
+++ ChGK.Droid/Bootstrap/EmailPluginBootstrap.cs
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace ChGK.Droid.Bootstrap
+{
+    public class EmailPluginBootstrap
+        : MvxPluginBootstrapAction<Cirrious.MvvmCross.Plugins.Email.PluginLoader>
+    {
+    }
+}
