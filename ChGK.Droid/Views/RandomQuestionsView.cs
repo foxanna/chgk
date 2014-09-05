680341133408d15cfb23f55c81e09dd9e458a77e
foxanna <anna.domashych@gmail.com>
2014-09-05 15:52:31 +0300

    Scroll random questions list to top after data update

--- ChGK.Droid/Views/RandomQuestionsView.cs
+++ ChGK.Droid/Views/RandomQuestionsView.cs
@@ -1,5 +1,9 @@
 using Android.Views;
+using Android.Widget;
+using ChGK.Core.Models;
 using ChGK.Core.ViewModels;
+using System.Collections.Generic;
+using Cirrious.MvvmCross.Binding.BindingContext;
 
 namespace ChGK.Droid.Views
 {
@@ -22,11 +26,36 @@ namespace ChGK.Droid.Views
         {
             switch (item.ItemId)
             {
-                case Resource.Id.refresh:
+                case Resource.Id.refresh:                    
                     (ViewModel as RandomQuestionsViewModel).RefreshCommand.Execute(null);
                     return true;
                 default:
                     return base.OnOptionsItemSelected(item);
+            }
+        }
+
+        ListView _listView;
+
+        public override void OnViewCreated(View view, Android.OS.Bundle savedInstanceState)
+        {
+            base.OnViewCreated(view, savedInstanceState);
+
+            _listView = view.FindViewById<ListView>(Resource.Id.items);
+
+            var bindingSet = this.CreateBindingSet<RandomQuestionsView, RandomQuestionsViewModel>();
+            bindingSet.Bind(this).For(n => n.Data).To(vm => vm.Questions);
+            bindingSet.Apply();
+        }
+
+        public List<IQuestion> Data
+        {
+            get
+            {
+                return null;
+            }
+            set
+            {
+                _listView.SetSelectionAfterHeaderView();
             }
         }
 	}
